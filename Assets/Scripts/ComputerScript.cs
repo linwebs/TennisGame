50931301e9ba9eb326e2f9aaf3aaefc6dc150bec
linwebs <thomas881114@gmail.com>
2021-06-27 02:34:23 +0800

    角色與電腦靜止動畫

--- Assets/Scripts/ComputerScript.cs
+++ Assets/Scripts/ComputerScript.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ComputerScript : MonoBehaviour {
+	public Animator anim;
+
 	void Start() {
+		anim = this.GetComponent<Animator>();
+		anim.Play("stand");
 
 	}
 
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,12 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour {
-	void Start() {
+public class PlayerScript : MonoBehaviour 
+{
+    public Animator anim;
+    float speed;
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+        anim.Play("stand");
+        speed = 3f;
+    }
 
-	}
-
-	void Update() {
-
-	}
+	void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, 0, speed * Time.deltaTime), Space.Self);  //前
+            anim.Play("running");
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime), Space.Self);  //後
+            anim.Play("running");
+        }
+        if (Input.GetKey(KeyCode.A) | Input.GetKey(KeyCode.LeftArrow)) //左
+        {
+            this.transform.Translate(Vector3.right * -speed * Time.deltaTime);
+            anim.Play("running");
+        }
+        if (Input.GetKey(KeyCode.D) | Input.GetKey(KeyCode.RightArrow)) //右
+        {
+            this.transform.Translate(Vector3.right * speed * Time.deltaTime);
+            anim.Play("running");
+        }
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+            anim.Play("stand");
+    }
 }
